110b7d90caa849ba2ba6fd76fd84dd339a550e44
hH-13 <62644047+hH-13@users.noreply.github.com>
2023-12-09 04:03:24 +0530

    an alternate implementation

--- map_of_india/map.c
+++ map_of_india/map.c
@@ -3,14 +3,14 @@
 main()
 {
     int a,b,c;
-    //int count = 1;
+    int count = 1;
     for (b=c=10;a="- FIGURE?, UMKC,XYZHello Folks,\
-    TFy.QJu ROo TNn(ROo)SLq SLq ULo+\
+    TFy!QJu ROo TNn(ROo)SLq SLq ULo+\
     UHs UJq TNn*RPn/QPbEWS_JSWQAIJO^\
     NBELPeHBFHT}TnALVlBLOFAkHFOuFETp\
     HCStHAUFAgcEAelclcn^r^r\\tZvYxXy\
     T|S~Pn SPm SOn TNn ULo0ULo#ULo-W\
-    Hq.WFs XDt!" [b+++21]; )
+    Hq!WFs XDt!" [b+++21]; )
     for(; a-- > 64 ; )
-    putchar ( ++c=='Z' ? c = c/ 9:35^b&1);
+    putchar ( ++c=='Z' ? c = c/ 9:33^b&1);
 }
